03e966b6a142de54ae30f337e4859a4516a1e7ba
realjck <jckleinbourg@gmail.com>
2023-01-09 04:32:22 +0100

    Change images and texts

--- assets/data/ui-texts.js
+++ assets/data/ui-texts.js
@@ -2,12 +2,12 @@ var UI_TEXTS = {};
 
 UI_TEXTS["en"] = {
 	"baseline": "Book of Changes<br>companion app",
-	"consigne": "First, ask your question<br><br>then flip three coins and click on the obtained result 6 times",
+	"consigne": "To consult the oracle, take three identical coins. State your question explicitly. Then perform the three-coin toss and click below on the result obtained 6x in a row.",
 	"book": "Click on the book to access the text"
 };
 
 UI_TEXTS["fr"] = {
 	"baseline": "Oracle Yi King<br>en images",
-	"consigne" : "En premier, posez votre question<br><br>puis lancez trois pièces et cliquez sur le résultat obtenu 6 fois de suite",
+	"consigne" : "Pour consulter l'oracle, munissez-vous de trois pièces de monnaie identiques. Formulez votre question de façon explicite. Effectuez ensuite le lancer des trois pièces et cliquez ci-dessous sur le résultat obtenu 6x de suite.",
 	"book": "Cliquez sur le livre pour accéder au texte"
 };
